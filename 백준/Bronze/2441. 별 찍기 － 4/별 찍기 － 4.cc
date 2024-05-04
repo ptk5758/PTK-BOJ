4a3a94b7cf5c964ac2a3445d6f30b02154aaf54a
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-05-04 15:53:55 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+int main() {
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j > 0; j--) {
+			std::cout << " ";
+		}
+		for (int j = n; j > i; j--) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
