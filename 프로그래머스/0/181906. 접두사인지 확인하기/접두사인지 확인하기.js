ef8c3db163b52bc16314af3f8d9bc8a293d2f08e
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-08-08 20:09:07 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_prefix) {
+    var answer = 0;
+    for (let i=0; i<is_prefix.length; i++) {
+        if (my_string[i] !== is_prefix[i]) return 0
+    }
+    return 1
+}
