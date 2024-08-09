1d91745a1fbdcbc2d971ef2c0947788d5497718c
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-08-09 23:15:26 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,14 @@
+function solution(dot) {
+    if (dot[0] > 0) { // 1, 4
+        if (dot[1] > 0)
+            return 1
+        else
+            return 4
+        
+    } else { // 2, 3
+        if (dot[1] > 0)
+            return 2
+        else
+            return 3
+    }
+}
