a81763f2f2122a8d9440b01a6c08d3723dd37738
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-09-07 09:10:51 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int *arr = new int[3];
+    for (int i=0; i<3; i++) {
+        std::cin >> arr[i];
+    }
+    std::sort(arr, arr + 3);
+    for (int i=0; i<3; i++) {
+        std::cout << arr[i] << " ";
+    }
+    
+    delete [] arr;
+    
+}
