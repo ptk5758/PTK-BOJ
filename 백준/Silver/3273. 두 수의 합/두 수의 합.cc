9de22e42d5ce250c2f608c37340857dab3ca25b5
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-01 00:33:53 +0900

    [Silver III] Title: 두 수의 합, Time: 964 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <algorithm>
-
-int main() {
-    int a; std::cin >> a;
-    int *arr = new int[a];
-    for (int i=0; i<a; i++) {
-        std::cin >> arr[i];
-    }
-    std::sort(arr, arr + a);
-    int x; std::cin >> x;
-    int result = 0, l = 0, r = a - 1;
-    while (l < r) {
-        if (arr[l] + arr[r] == x) {
-            result++;
-            l++;
-        } else if (arr[l] + arr[r] < x) {
-            l++;
-        } else if (arr[l] + arr[r] > x) {
-            r--;
-        }
-    }
-    std::cout << result << "\n";
-    
-    return 0;
+int main()
+{
+	int n; std::cin >> n;
+	int* arr = new int[n + 1];
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
+	int x; std::cin >> x;
+	int result = 0;
+	std::sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		if (arr[i] >= x) break;
+		for (int j = i + 1; j < n; j++) {
+			int value = arr[i] + arr[j];
+			if (value == x) {
+				/*std::cout << arr[i] << "," << arr[j] << "\n";*/
+				result++;
+			} else if (value > x) {
+				break;
+			}
+		}
+	}
+	std::cout << result << "\n";
+	return 0;
 }
