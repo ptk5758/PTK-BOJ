cc9ceea4f6d8ae3c56e586d85b2d1a80e061e906
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-03 02:06:02 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,21 +1,64 @@
 #include <iostream>
-#include <map>
-
-int main() {
+#include <algorithm>
+#include <vector>
+#define MIN -10000000
+#define MAX 10000000
+typedef std::vector<int> List;
+int lowerBound(List &list, int value)
+{
+	int start = 0;
+	int end = list.size();
+	while (start < end)
+	{
+		int mid = start + (end - start) / 2;
+		if (list[mid] < value)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid;
+		}
+	}	
+	return start;
+}
+int upperBound(List &list, int value)
+{
+	int start = 0;
+	int end = list.size();
+	while (start < end)
+	{
+		int mid = start + (end - start) / 2;
+		if (list[mid] <= value)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid;
+		}
+	}	
+	return start;
+}
+int main()
+{
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	int N, M, i, value;
-	std::map<int, int> map;	
-	std::cin >> N;
-	for (i = 0; i < N; i++) {		
-		std::cin >> value;
-		if (map[value]) map[value]++;
-		else map[value] = 1;
+	std::cout.tie(NULL);
+	int n; std::cin >> n;
+	List list;	
+	for (int i = 0; i < n; i++)
+	{
+		int value; std::cin >> value;
+		list.push_back(value);
 	}
-
-	std::cin >> M;
-	for (i = 0; i < M; i++) {
-		std::cin >> value;
-		std::cout << map[value] << " ";		
+	std::sort(list.begin(), list.end());
+	int m; std::cin >> m;
+	while (m--)
+	{
+		int value; std::cin >> value;
+		std::cout << upperBound(list, value) - lowerBound(list, value) << " ";
 	}
+	
+	return 0;
 }
