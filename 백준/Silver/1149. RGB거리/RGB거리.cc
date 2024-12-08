9b56f55cd53370fc2bcbb1042042dcd9d8882ada
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-09 00:57:54 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,54 +1,38 @@
 #include <iostream>
-int main() {
-    // 입력
-    int n;
-    std::cin >> n;
-    
-    // 메모리 할당
-    int **arr = new int*[3];
-    int **d = new int*[3];
-    for (int i=0; i<3; i++) {
-        arr[i] = new int[n];
-        d[i] = new int[n] {0,};
-    }
-//    int arr[3][1001];
-//    int d[3][1001];
-    
-    // 값 삽입
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<3; j++) {
-            std::cin >> arr[j][i];
-            if (i == 0) {
-                d[j][i] = arr[j][i];
-            }
-        }
-    }
-    for (int i=1; i<n; i++) {
-        d[0][i] = (d[1][i-1] > d[2][i-1] ? d[2][i-1] : d[1][i-1]) + arr[0][i];
-        d[1][i] = (d[0][i-1] > d[2][i-1] ? d[2][i-1] : d[0][i-1]) + arr[1][i];
-        d[2][i] = (d[0][i-1] > d[1][i-1] ? d[1][i-1] : d[0][i-1]) + arr[2][i];
-    }
-    if (d[0][n-1] > d[1][n-1]) {
-        if (d[1][n-1] > d[2][n-1]) {
-            std::cout << d[2][n-1];
-        } else {
-            std::cout << d[1][n-1];
-        }
-    } else {
-        if (d[0][n-1] > d[2][n-1]) {
-            std::cout << d[2][n-1];
-        } else {
-            std::cout << d[0][n-1];
-        }
-    }
-    
-    // 메모리 헤제
-    for (int i=0; i<3; i++) {
-        delete [] arr[i];
-        delete [] d[i];
-    }
-    delete [] arr;
-    delete [] d;
-    
-    return 0;
+#include <vector>
+#include <algorithm>
+
+typedef std::vector<int> Vector;
+
+void Solve()
+{
+	int n; std::cin >> n;
+	std::vector<Vector> d(3, Vector(n + 1, 0)), cost(3, Vector(n + 1, 0 ));
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < 3; j++) 
+		{
+			int value; std::cin >> value;
+			cost[j][i] = value;
+		}
+	}
+	d[0][0] = cost[0][0];
+	d[1][0] = cost[1][0];
+	d[2][0] = cost[2][0];
+	for (int i = 1; i <= n; i++)
+	{
+		d[0][i] = std::min(d[1][i - 1], d[2][i - 1]) + cost[0][i];
+		d[1][i] = std::min(d[0][i - 1], d[2][i - 1]) + cost[1][i];
+		d[2][i] = std::min(d[0][i - 1], d[1][i - 1]) + cost[2][i];
+	}
+	int result = std::min(d[0][n], d[1][n]);
+	std::cout << std::min(result, d[2][n]) << "\n";
+}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	Solve();
+	return 0;
 }
