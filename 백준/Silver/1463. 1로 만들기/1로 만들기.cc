a703f7733d1ef4ca04ce4e78defbb9ad31c7d5ea
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-08 20:17:56 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,25 +1,16 @@
 #include <iostream>
-
-int d[1000001]= {0, };
-int min(int a, int b) {
-    return a > b ? b : a;
-}
-int main() {
-    int n;
-    std::cin >> n;
-    d[0] = 0;
-    d[1] = 0;
-    d[2] = 1;
-    d[3] = 1;
-    for (int i=4; i<=n; i++) {
-        d[i] = d[i-1] + 1;
-        if (i % 3 == 0) {
-            d[i] = min(d[i], d[i/3] + 1);
-        }
-        if (i % 2 == 0) {
-            d[i] = min(d[i], d[i/2] + 1);
-        }
-    }
+#include <algorithm>
+#include <vector>
+int main()
+{
+    int n; std::cin >> n;
+    std::vector<int> d(n + 1);
+    for (int i=2; i<=n; i++)
+    {
+        d[i] = d[i - 1] + 1;
+        if (i % 2 == 0) d[i] = std::min(d[i], d[i / 2] + 1);
+        if (i % 3 == 0) d[i] = std::min(d[i], d[i / 3] + 1);
+    }    
     std::cout << d[n] << "\n";
     return 0;
 }
