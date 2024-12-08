187d595b8492510944215d62cacef93cee35471d
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-08 20:34:10 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,18 +1,24 @@
 #include <iostream>
-int d[11] = {0, 1, 2, 4, };
-int solve(int n) {
-    for (int i=4; i<=n; i++) {
-        d[i] = d[i-1] + d[i-2] + d[i-3];
-    }
-    return d[n];
+#include <algorithm>
+#include <vector>
+std::vector<int> d(1000001);
+int Solution(int value)
+{
+    if (d[value]) return d[value];
+    return d[value] = Solution(value - 1) + Solution(value - 2) + Solution(value - 3);
 }
-int main() {
-    int t;
-    std::cin >> t;
-    while (t--) {
-        int value;
-        std::cin >> value;
-        std::cout << solve(value) << "\n";
+
+int main()
+{
+    int n; std::cin >> n;
+    d[0] = 0;
+    d[1] = 1;
+    d[2] = 2;
+    d[3] = 4;
+    for (int i=0; i<n; i++)
+    {
+        int value; std::cin >> value;    
+        std::cout << Solution(value) << "\n";    
     }
     return 0;
 }
