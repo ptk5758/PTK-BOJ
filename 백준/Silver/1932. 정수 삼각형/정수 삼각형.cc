b6c31190039e9857a5c44c8928902f3dcb2b6129
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-09 17:56:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
-#include <string>
+#include <algorithm>
 
-int arr[501][502] = { 0, };
-std::vector<std::string> split(std::string str) {
-	std::stringstream ss(str);
-	std::vector<std::string> result;
-	std::string temp;
-	while (std::getline(ss, temp, ' ')) {
-		result.push_back(temp);
-	}
-	return result;
-}
-int main() {	
-	std::string str;
-	std::getline(std::cin, str);
-	int n = std::stoi(str);
-	for (int i = 0; i < n; i++) {
-		std::getline(std::cin, str);
-		std::vector<std::string> nums = split(str);
-		for (int j = 0; j < nums.size(); j++) {
-			arr[i][j] = std::stoi(nums[j]);
-		}
-	}
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 0; j < i; j++) {
-			int max;
-			if (arr[i][j] > arr[i][j + 1])
-				max = arr[i][j];
-			else
-				max = arr[i][j + 1];
-			arr[i - 1][j] += max;
-		}
-	}
-	std::cout << arr[0][0];
-	return 0;
+int cost[501][501] = {};
+int d[501][501] = {};
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n; std::cin >> n;
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            std::cin >> cost[i][j];
+        }
+    }
+    d[1][1] = cost[1][1];
+
+    for (int i=1; i<=n - 1; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            d[i+1][j] = std::max(d[i+1][j], d[i][j] + cost[i+1][j]);
+            d[i+1][j+1] = d[i][j] + cost[i+1][j+1];
+        }
+    }
+    int max = -1;
+    for (int i=1; i<=n; i++)
+    {
+        if (max < d[n][i]) max = d[n][i];
+    }    
+    std::cout << max << "\n";
+    return 0;
 }
