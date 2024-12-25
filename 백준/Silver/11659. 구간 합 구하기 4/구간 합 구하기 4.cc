20dbf2ec2ad8a03ba0f2013da8307a523fbaeeee
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-25 22:52:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,25 +1,21 @@
 #include <iostream>
-
-int arr[100001] = { 0, };
-void Solve()
-{
-    int n, m; std::cin >> n >> m;
-    for (int i=1; i<=n; i++) {
-        std::cin >> arr[i];
-        arr[i] += arr[i - 1];
-    }
-    for (int i=0; i<m; i++)
-    {
-        int start, end; std::cin >> start >> end;
-        std::cout << arr[end] - arr[start - 1] << "\n";
-    }
-
-}
+#include <vector>
+using List = std::vector<int>;
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-    Solve();
-    return 0;
+	std::ios::sync_with_stdio(false);
+	std::cout.tie(NULL);
+	std::cin.tie(NULL);
+	int n, m; std::cin >> n >> m;
+	List arr(n + 1), d(n + 1);
+	for (int i = 1; i <= n; i++) {
+		std::cin >> arr[i];
+		d[i] = d[i - 1] + arr[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		int s, e; std::cin >> s >> e;
+
+		std::cout << d[e] - d[s - 1] << "\n";
+	}
 }
