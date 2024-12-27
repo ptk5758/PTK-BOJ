78c9bd5aeee830fa9b99d8dc955e8ed11de669f0
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2024-12-27 15:48:15 +0900

    [Gold V] Title: 탑, Time: 92 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,32 +1,28 @@
 #include <iostream>
-#include "stack"
-typedef std::pair<int, int> Pair; // index, value
-typedef std::stack<Pair> Stack;
-int Solve(Stack &s, Pair p) {
-    while (!s.empty()) {
-        if (s.top().second >= p.second) {
-            int result = s.top().first + 1;
-            s.push(p);
-            return result;
-        } else {
-            s.pop();
-        }
-    }
-    s.push(p);
-    return 0;
-}
+#include <stack>
 
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
-    int n; std::cin >> n;
-    Stack s;
-    for (int i=0; i<n; i++) {
-        int value; std::cin >> value;
-        std::cout << Solve(s, {i, value}) << " ";
-    }
-    std::cout << "\n";
-    
-    return 0;
+using Pair = std::pair<int, int>;
+using Stack = std::stack<Pair>;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
+	int n; std::cin >> n;
+	Stack s;	
+	for (int i=1; i<=n; i++) {
+		int value; std::cin >> value;
+		if (s.empty()) {
+			s.push({i, value});
+			std::cout << 0 << " ";
+		} else {
+			while (!s.empty() && s.top().second < value) {
+				s.pop();
+			}
+			std::cout << (s.empty() ? 0 : s.top().first) << " ";
+			s.push({i, value});
+		}
+	}
+	std::cout << "\n";
+	return 0;
 }
