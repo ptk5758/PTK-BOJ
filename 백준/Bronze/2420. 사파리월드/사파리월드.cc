6cd0bee8efb2777d50bfe0739b41e247b05fa30e
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2025-01-29 01:18:50 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    long long a,b; std::cin >> a >> b;
+    long long result = std::abs(a - b);
+    std::cout << result << "\n";
+    return 0;
+}
