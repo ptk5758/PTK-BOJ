6d51d4bf5679de7240f84ecda55d50ef2967b0be
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2025-02-21 23:50:28 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << ".  .   ." << "\n";
+	std::cout << "|  | _ | _. _ ._ _  _" << "\n";
+	std::cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";
+	
+	return 0;
+}
