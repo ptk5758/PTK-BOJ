2381e27747e253fe62a2273a8fdc21c15939ea32
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2025-03-09 23:24:56 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    bool a; std::cin >> a;
+    if (a) {
+        std::cout << "Leading the Way to the Future" << "\n";
+    }
+    else {
+        std::cout << "YONSEI" << "\n";
+    }
+    return 0;
+}
